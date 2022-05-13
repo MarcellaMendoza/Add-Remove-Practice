c8f603565b6d8cb67c00d809606007dbd21e1c5e
MarcellaMendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-13 03:00:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstName = "Marcella "
+let lastName = "Mendoza"
+let fullName = firstName + lastName
+console.log(fullName)
+
+let name = "Kirk"
+let greeting = "Hi there"
+let fullGreeting = greeting + "," + " Captain " + name + "!"
+console.log(fullGreeting)
+
+let myPoints = 2
+let bonusPoints = 0
+let remPoint = 0
+
+function addBonusPoints () {
+  myPoints = myPoints + 5
+  console.log(myPoints)
+}
+
+function removePoint() {
+   myPoints = myPoints - 1
+  console.log(myPoints)
+
+}
+
+console.log("2" + 6) // 26
+console.log(21 + 7) //28
+console.log(6 + "43") // 643
+console.log("My score: " + 8 + 8) // My score: 88
+console.log(2 + 2) // 4
+console.log("11" + "15" + 22) // 111522
+
